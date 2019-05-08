a3794ede54f88640077ceaaf649f4924e827137b
Asunqingwen <sqw123az@sina.com>
2019-05-08 18:17:16 +0800

    LixingerSpider

---
+++ LixingerSpider/logging_job.py
@@ -0,0 +1,26 @@
+import logging
+from logging import handlers
+
+LOG_LEVEL = logging.INFO  # 日志等级
+
+
+def get_logger():
+	"""
+	创建日志实例
+	"""
+	formatter = logging.Formatter("%(asctime)s - %(message)s")
+	logger = logging.getLogger("monitor")
+	logger.setLevel(LOG_LEVEL)
+
+	cmd = logging.StreamHandler()
+	cmd.setFormatter(formatter)
+
+	file = handlers.TimedRotatingFileHandler(filename="./log", when='D', backupCount=30, encoding='utf-8')
+	file.setFormatter(formatter)
+
+	logger.addHandler(cmd)
+	logger.addHandler(file)
+	return logger
+
+
+logger = get_logger()
--- LixingerSpider/spider_job.py
+++ LixingerSpider/spider_job.py
@@ -1,39 +1,60 @@
+import csv
 import json
 
 import requests
 
-url = "https://www.lixinger.com/api/analyt/stock-collection/price-metrics/indices/latest"
+from logging_job import logger
+
+data_list_url = "https://www.lixinger.com/api/analyt/stock-collection/price-metrics/indices/latest"
+detail_csv_url = "https://www.lixinger.com/api/analyt/stock-collection/price-metrics/load"
 payloadHeader = {
 	'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36',
-	'Cookie': 'jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1Y2QxMzVhZGMxOTZkYTUyOTAwZTlkYTAiLCJpYXQiOjE1NTcyMTQ2MzcsImV4cCI6MTU1NzgxOTQzN30.4cOWbqtL1nCPZ_TxOBC3bSO8h8LJH3M2MyygucOssxE; Hm_lvt_ec0ee7e5c8bed46d4fdf3f338afc08f5=1557214618,1557214637; Hm_lpvt_ec0ee7e5c8bed46d4fdf3f338afc08f5=1557219702',
+	"Content-Type": "application/json;charset=UTF-8",
+	'Cookie': 'jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1Y2QxMzVhZGMxOTZkYTUyOTAwZTlkYTAiLCJpYXQiOjE1NTcyMTQ2MzcsImV4cCI6MTU1NzgxOTQzN30.4cOWbqtL1nCPZ_TxOBC3bSO8h8LJH3M2MyygucOssxE; Hm_lvt_ec0ee7e5c8bed46d4fdf3f338afc08f5=1557214618,1557214637,1557280317; Hm_lpvt_ec0ee7e5c8bed46d4fdf3f338afc08f5=1557281309',
 }
-payloadData = {"metricNames": ["pe_ttm", "pb", "ps_ttm", "dyr"], "granularities": ["y_10"],
-               "metricTypes": ["weightedAvg"]}
-r = requests.post(url, data=json.dumps(payloadData), headers=payloadHeader)
-data_list = json.loads(r.text)
-getHeader = {
-	"Cookie": "jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1Y2QxMzVhZGMxOTZkYTUyOTAwZTlkYTAiLCJpYXQiOjE1NTcyMTQ2MzcsImV4cCI6MTU1NzgxOTQzN30.4cOWbqtL1nCPZ_TxOBC3bSO8h8LJH3M2MyygucOssxE; Hm_lvt_ec0ee7e5c8bed46d4fdf3f338afc08f5=1557214618,1557214637; Hm_lpvt_ec0ee7e5c8bed46d4fdf3f338afc08f5=1557221422",
-	"User-Agent": " Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36",
-}
-base_url = "https://www.lixinger.com/analytics/indice/sh/{}/detail/value?metric-name={}&metric-type=weightedAvg&granularity=f_s"
-csv_url = "https://www.lixinger.com/api/analyt/stock-collection/price-metrics/load"
-csv_payloadHeader = {
-"Cookie": "jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1Y2QxMzVhZGMxOTZkYTUyOTAwZTlkYTAiLCJpYXQiOjE1NTcyMTQ2MzcsImV4cCI6MTU1NzgxOTQzN30.4cOWbqtL1nCPZ_TxOBC3bSO8h8LJH3M2MyygucOssxE; Hm_lvt_ec0ee7e5c8bed46d4fdf3f338afc08f5=1557214618,1557214637; Hm_lpvt_ec0ee7e5c8bed46d4fdf3f338afc08f5={}",
-	"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36"
-}
-pe_ttm_csv_payloadData = {"stockIds": [10000000009], "dateFlag": "week", "granularity": "f_s",
+data_list_payloadData = {"metricNames": ["pe_ttm", "pb", "ps_ttm", "dyr"], "granularities": ["y_10"],
+                         "metricTypes": ["weightedAvg"]}
+
+pe_ttm_csv_payloadData = {"stockIds": "", "dateFlag": "week", "granularity": "f_s",
                           "metricNames": ["pe_ttm", "cp", "mc", "cmc"], "metricTypes": ["weightedAvg"]}
-pb_csv_payloadData = {"stockIds": [10000000009], "dateFlag": "week", "granularity": "f_s",
+pb_csv_payloadData = {"stockIds": "", "dateFlag": "week", "granularity": "f_s",
                       "metricNames": ["pb", "cp", "mc", "cmc"], "metricTypes": ["weightedAvg"]}
 
-r_csv = requests.post(csv_url, data=json.dumps(pe_ttm_csv_payloadData), headers=csv_payloadHeader)
-# data_list = json.loads(r_csv.text)
-pe_ttm_url = base_url.format(data_list[0]["stockCode"],'pe_ttm')
-cookies_url = "https://www.lixinger.com/api/analyt/stock-collection/price-metrics/get-price-metrics-chart-info"
-r = requests.get(cookies_url)
-print(r.cookies.get_dict())
-# for data in data_list:
-# 	get_url = base_url.format(data["stockCode"])
-# 	pe_ttm_url = base_url.format(data["stockCode"], 'pe_ttm')
-# 	pb_url = base_url.format(data["stockCode"], 'pb')
-# 	print(get_url)
+
+def get_csv():
+	r = requests.post(data_list_url, data=json.dumps(data_list_payloadData), headers=payloadHeader)
+	data_list = json.loads(r.text)
+	for data in data_list:
+		zhishu_name = data["cnName"]
+		logger.info("{}的PE和PB数据正在收集........".format(zhishu_name))
+		pe_ttm_csv_payloadData["stockIds"] = [data["stockId"]]
+		pb_csv_payloadData["stockIds"] = [data["stockId"]]
+		try:
+			pe_ttm_csv = requests.post(detail_csv_url, data=json.dumps(pe_ttm_csv_payloadData), headers=payloadHeader)
+			pb_csv = requests.post(detail_csv_url, data=json.dumps(pb_csv_payloadData), headers=payloadHeader)
+			pe_ttm_csv_data = json.loads(pe_ttm_csv.text)
+			pb_csv_data = json.loads(pb_csv.text)
+
+			pe_ttm_csv_data = reversed(pe_ttm_csv_data)
+			pb_csv_data = reversed(pb_csv_data)
+
+			headers = ['date', 'pe_ttm', "pb"]
+			with open("./CsvFiles/{}.csv".format(zhishu_name), "w", newline="") as f:
+				f_csv = csv.DictWriter(f, headers)
+				f_csv.writeheader()
+				if zhishu_name != "恒生指数":
+					for pe_ttm_data, pb_data in zip(pe_ttm_csv_data, pb_csv_data):
+						row = dict(
+							zip(headers, [pe_ttm_data['date'].split("T")[0], pe_ttm_data["pe_ttm"]["weightedAvg"],
+							              pb_data["pb"]["weightedAvg"]]))
+						f_csv.writerow(row)
+				else:
+					for pe_ttm_data, pb_data in zip(pe_ttm_csv_data, pb_csv_data):
+						if pe_ttm_data["pe_ttm"]:
+							row = dict(
+								zip(headers, [pe_ttm_data['date'].split("T")[0], pe_ttm_data["pe_ttm"]["weightedAvg"],
+								              pb_data["pb"]["weightedAvg"]]))
+							f_csv.writerow(row)
+		except Exception as e:
+			logger.error(e)
+		logger.info("{}的PE和PB数据已完成收集........".format(zhishu_name))
